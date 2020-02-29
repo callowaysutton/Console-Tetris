798412a04dbf9010683033055206f0daf063095e
Calloway Sutton <calloway@myself.com>
2020-02-29 17:26:42 -0500

    kinda broken

--- Console Tetris.cpp
+++ Console Tetris.cpp
@@ -7,7 +7,30 @@ using namespace std;
 
 wstring shapes[7];
 
-int Setup() {
+//Setup Vars
+int ScreenWidth;
+int ScreenHeight;
+int FieldWidth = 12;
+int FieldHeight = 18;
+unsigned char* Field = nullptr;
+
+
+//Logistics Vars
+bool Key[4];
+int CurrentPiece = 0;
+int CurrentRotation = 0;
+int CurrentX = FieldWidth / 2;
+int CurrentY = 0;
+int Speed = 20;
+int SpeedCount = 0;
+bool ForceDown = false;
+bool RotateHold = true;
+int PieceCount = 0;
+int Score = 0;
+vector<int> Lines;
+bool GameOver = false;
+
+void Setup() {
 	//Create the shapes 
 	//Stick
 	shapes[0].append(L"..X.");
@@ -50,11 +73,70 @@ int Setup() {
 	shapes[6].append(L".XX.");
 	shapes[6].append(L".X..");
 	shapes[6].append(L".X..");
+
+	wchar_t* screen = new wchar_t[ScreenWidth * ScreenHeight];
+	for (int i = 0; i < ScreenWidth * ScreenHeight; i++) screen[i] = L' ';
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	Field = new unsigned char[FieldWidth * FieldHeight];
+	for (int x = 0; x < FieldWidth; x++)
+		for (int y = 0; y < FieldHeight; y++)
+			Field[y * FieldWidth + x] = (x == 0 || x == FieldWidth - 1 || y == FieldHeight - 1) ? 9 : 0;
+}
+
+void Input() {
+	for (int k = 0; k < 4; k++)								// R   L   D Z
+		Key[k] = (0x8000 & GetAsyncKeyState((unsigned char)("\x27\x25\x28Z"[k]))) != 0;
+}
+
+void Draw() {
+	for (int x = 0; x < FieldWidth; x++)
+		for (int y = 0; y < FieldHeight; y++)
+			screen[(y + 2) * ScreenWidth + (x + 2)] = L" ABCDEFG=#"[Field[y * FieldWidth + x]];
+
+	// Draw Current Piece
+	for (int px = 0; px < 4; px++)
+		for (int py = 0; py < 4; py++)
+			if (shapes[CurrentPiece][Rotate(px, py, CurrentRotation)] != L'.')
+				screen[(CurrentY + py + 2) * ScreenWidth + (CurrentX + px + 2)] = CurrentPiece + 65;
+
+	// Draw Score
+	swprintf_s(&screen[2 * ScreenWidth + FieldWidth + 6], 16, L"SCORE: %8d", Score);
+
+	// Animate Line Completion
+	if (!Lines.empty())
+	{
+		// Display Frame (cheekily to draw lines)
+		WriteConsoleOutputCharacter(hConsole, screen, ScreenWidth * ScreenHeight, { 0,0 }, &dwBytesWritten);
+		this_thread::sleep_for(400ms); // Delay a bit
+
+		for (auto& v : Lines)
+			for (int px = 1; px < FieldWidth - 1; px++)
+			{
+				for (int py = v; py > 0; py--)
+					Field[py * FieldWidth + px] = Field[(py - 1) * FieldWidth + px];
+				Field[px] = 0;
+			}
+
+		Lines.clear();
+	}
+
+	// Display Frame
+	WriteConsoleOutputCharacter(hConsole, screen, ScreenWidth * ScreenHeight, { 0,0 }, &dwBytesWritten);
+
 }
 
 int main()
 {
 	Setup();
-	cout << "Hello CMake." << endl;
+	while (!GameOver) {
+		Input();
+		Draw();
+		this_thread::sleep_for(50ms); // Small Step = 1 Game Tick
+		SpeedCount++;
+		ForceDown = (SpeedCount == Speed);
+	}
 	return 0;
 }
--- Console Tetris.h
+++ Console Tetris.h
@@ -4,5 +4,46 @@
 #pragma once
 
 #include <iostream>
+#include <Windows.h>
+#include <thread>
+#include <vector>
+#include <stdio.h>
 
-// TODO: Reference additional headers your program requires here.
+int Rotate(int sx, int sy, int r) {
+	switch (r % 4) {
+	case 0: return sy * 4 + sx;
+	case 1: return 12 + sy - (sx * 4);
+	case 2: return 15 - (sy * 4) - sx;
+	case 3: return 3 - sy + (sx * 4);
+	}
+}
+
+bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+{
+	// All Field cells >0 are occupied
+	for (int px = 0; px < 4; px++)
+		for (int py = 0; py < 4; py++)
+		{
+			// Get index into piece
+			int pi = Rotate(px, py, nRotation);
+
+			// Get index into field
+			int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+			// Check that test is in bounds. Note out of bounds does
+			// not necessarily mean a fail, as the long vertical piece
+			// can have cells that lie outside the boundary, so we'll
+			// just ignore them
+			if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
+			{
+				if (nPosY + py >= 0 && nPosY + py < nFieldHeight)
+				{
+					// In Bounds so do collision check
+					if (tetromino[nTetromino][pi] != L'.' && pField[fi] != 0)
+						return false; // fail on first hit
+				}
+			}
+		}
+
+	return true;
+}
